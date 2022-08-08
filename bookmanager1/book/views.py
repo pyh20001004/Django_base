501742ac3980df2f95d08aead683f497cf760494
pyh20001004 <1580525601@qq.com>
2022-08-08 19:27:23 +0800

    数据库操作

--- bookmanager1/book/views.py
+++ bookmanager1/book/views.py
@@ -28,8 +28,12 @@ def index(request):
 # redis 的数据存储在 内存
 # 我把硬盘的数据保存在内存 也称之为 缓存
 
-####################增加数据########################
 from book.models import BookInfo
+from book.models import PeopleInfo
+
+
+
+####################增加数据########################
 # 方式一
 book=BookInfo(
     name='Django',
@@ -49,6 +53,7 @@ BookInfo.objects.create(
 # 直接将数据保存到了数据库中
 
 
+
 ####################修改数据########################
 # 方式1
 # select * from bookinfo where id=6
@@ -64,6 +69,7 @@ BookInfo.objects.filter(id=6).update(name='爬虫入门',commentcount=666)
 # BookInfo.objects.get(id=5).update(name='5555',commentcount=999)
 
 
+
 ####################删除数据########################
 # 删除分两种:
     # 物理删除(这条记录的数据 删除)
@@ -77,6 +83,152 @@ BookInfo.objects.get(id=6).delete()
 BookInfo.objects.filter(id=5).delete()
 
 
+
+####################查询数据########################
+# get查询单一结果，如果不存在会抛出模型类.DoesNotExist异常
+try:
+    book=BookInfo.objects.get(id=1)
+except BookInfo.DoesNotExist:
+    print('查询结果不存在')
+
+# all查询多个结果
+BookInfo.objects.all()
+from book.models import PeopleInfo
+PeopleInfo.objects.all()
+
+# count查询结果数量
+BookInfo.objects.all().count()
+BookInfo.objects.count()
+
+
+
+###########################过滤查询##########################
+# 实现SQL中的where功能，包括
+#     filter过滤出多个结果
+#     exclude排除掉符合条件剩下的结果
+#     get过滤单一结果
+
+# 模型类名.objects.filter(属性名__运算符=值)       获取n个结果  n=0,1,2,...
+# 模型类名.objects.exclude(属性名__运算符=值)      获取n个结果  n=0,1,2,...
+# 模型类名.objects.get(属性名__运算符=值)          获取1个结果 或者 异常
+
+# 运算符:
+#     相等
+#         exact：表示判等
+#     模糊查询
+#         contains：是否包含
+#         startswith、endswith：以指定值开头或结尾
+#     空查询
+#         isnull：是否为null
+#     范围查询
+#         in：是否包含在范围内
+#     比较查询
+#         gt大于 (greater then)
+#         gte大于等于 (greater then equal)
+#         lt小于 (less then)
+#         lte小于等于 (less then equal)
+#         不等于的运算符，使用exclude()过滤器
+#     日期查询
+#         year、month、day、week_day、hour、minute、second：对日期时间类型的属性进行运算
+
+# 查询编号为1的图书
+book=BookInfo.objects.get(id=1)         #简写形式 （属性名=值）
+# exact：表示判等
+book=BookInfo.objects.get(id__exact=1)  #完整形式  (id__exact=1)
+# pk primary key 主键
+BookInfo.objects.get(pk=1)
+BookInfo.objects.get(id=1)
+BookInfo.objects.filter(id=1)
+
+# 查询书名包含'湖'的图书
+# contains：是否包含
+BookInfo.objects.filter(name__contains='湖')
+
+# 查询书名以'部'结尾的图书
+# startswith、endswith：以指定值开头或结尾
+BookInfo.objects.filter(name__endswith='部')
+
+# 查询书名为空的图书
+#     isnull：是否为null
+BookInfo.objects.filter(name__isnull=True)
+
+# 查询编号为1或3或5的图书
+# in：是否包含在范围内
+BookInfo.objects.filter(id__in=[1,3,5])
+
+# 查询编号大于3的图
+# gt大于 (greater then)
+# gte大于等于 (greater then equal)
+# lt小于 (less then)
+# lte小于等于 (less then equal)
+BookInfo.objects.filter(id__gt=3)
+
+# 查询编号不等于3的书籍
+# 不等于的运算符，使用exclude()过滤器
+BookInfo.objects.exclude(id=3)
+
+# 查询1980年发表的图书
+# year、month、day、week_day、hour、minute、second：对日期时间类型的属性进行运算
+BookInfo.objects.filter(pub_date__year=1980)
+
+# 查询1990年1月1日后发表的图书
+BookInfo.objects.filter(pub_date__gt='1990-1-1')
+# 错误的
+# BookInfo.objects.filter(pub_date__gt='199011')
+
+
+
+###########################F对象##############################
+# 导入F对象
+from django.db.models import F
+
+# 使用： 2个属性的比较
+#语法形式： 以filter为例  模型类名.objects.filter(属性名__运算符=F('第二个属性名'))
+
+# 查询阅读量大于等于评论量的图书
+BookInfo.objects.filter(readcount__gte=F('commentcount'))
+
+
+
+#########################并且查询#############################
+# 并且查询
+# 查询阅读量大于20，并且编号小于3的图书。
+BookInfo.objects.filter(readcount__gt=20).filter(id__lt=3)
+# 两种方式一致
+BookInfo.objects.filter(readcount__gt=20,id__lt=3)
+
+
+
+
+###########################Q对象##############################
+# 导入Q对象
+from django.db.models import Q
+
+# 或者查询
+# 或者语法：  模型类名.objects.filter(Q(属性名__运算符=值)|Q(属性名__运算符=值)|...)
+# 并且语法：  模型类名.objects.filter(Q(属性名__运算符=值)&Q(属性名__运算符=值)&...)
+# not 非 语法：  模型类名.objects.filter(～Q(属性名__运算符=值))
+
+# 查询阅读量大于20，或者编号小于3的图书。
+BookInfo.objects.filter(Q(readcount__gt=20)|Q(id__lt=3))
+
+# 查询编号不等于3的书籍
+BookInfo.objects.exclude(id=3)
+BookInfo.objects.filter(~Q(id=3))
+
+
+
+#############聚合函数#####################################
+
+from django.db.models import Sum,Max,Min,Avg,Count
+
+# 模型类名.objects.aggregate(Xxx('字段名'))
+
+BookInfo.objects.aggregate(Sum('readcount'))
+
+
+###############排序############################
+BookInfo.objects.all().order_by('readcount')
 
 
 
